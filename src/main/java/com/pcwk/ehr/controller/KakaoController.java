13f3758ef07f4c66db10ccdcfb9f611d6f832db6
yongjin1004 <yongjin7660@gmail.com>
2025-05-24 21:35:36 +0900

    kakaopolygon

--- src/main/java/com/pcwk/ehr/controller/KakaoController.java
+++ src/main/java/com/pcwk/ehr/controller/KakaoController.java
@@ -1,12 +1,8 @@
 package com.pcwk.ehr.controller;
 
-import com.pcwk.ehr.dto.kakao.KakaoDTO;
-import com.pcwk.ehr.service.KakaoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,9 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class KakaoController {
 
 
-    @GetMapping("/test")
+    @GetMapping("/polygon")
     public String kakao() {
-        return "test";
+        return "polygon";
     }
 
 }
